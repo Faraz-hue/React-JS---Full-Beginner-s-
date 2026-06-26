d43852c51b213a26ab539a94cad7ca062a7066d1
Admin <mfarazazam9@gmail.com>
2026-06-26 09:23:54 +0500

    Build a project Travel Jounel where we learned static data display on the screen. Later dynamic data using call back function mapin the component and passed as a properties, Later we see property passed as object and then with spread object

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,36 @@
 
 import Entry from './Entry'
+import data from '../data'
+
 export default function Main() {
+
+    const entryElements = data.map((entry) => {
+        return (
+            < Entry
+                // 1. Passing as an attribute properties
+                // id={entry.id}
+                //     img={entry.img}
+                //     title={entry.title}
+                //     country={entry.country}
+                //     googleMapsLinks={entry.googleMapsLink}
+                //     dates={entry.dates}
+                //     text={entry.text}
+
+                // 2. Passing as an object
+                // id={entry.id}
+                // entry={entry}
+                // 3. PAssing as spread object
+                id={entry.id}
+                {...entry}
+            />
+
+
+        )
+    })
     return (
         <main >
-            <Entry />
+            {entryElements}
+
         </main>
     );
 } 
