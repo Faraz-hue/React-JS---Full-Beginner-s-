2d4a1085343d27eb878c5227cad39eef18c01648
Admin <mfarazazam9@gmail.com>
2026-06-29 16:11:04 +0500

    add CSS cleaned and sections

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -12,16 +12,44 @@ export default function Body() {
 
     ))
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
+    function AddIngredeient(formData) {
         const newIngredient = formData.get("ingredient")
-        setIngredients(prevIngredient => [...prevIngredient, newIngredient])
+        setIngredients(prevIngredient =>
+            [...prevIngredient, newIngredient])
 
     }
+
+    function signup(formData) {
+        const data = Object.fromEntries(formData)
+        const dietry = formData.getAll("dietry")
+        const allData = {
+            ...data, dietry
+        }
+        console.log(allData)
+    }
+    // Conditional Rendering
+    const [unreadMessages, SetUnreadMessages] = React.useState([])
+
+    function determineText() {
+        if (unreadMessages.length === 0) {
+            return "You're all caught up"
+        }
+        else if (unreadMessages.length === 1) {
+            return "You have 1 unread message"
+        }
+        else {
+            return `You have ${unreadMessages.length} unread messages`
+        }
+    }
+    // End conditional Rendering
     return (
         <>
-            <form onSubmit={handleSubmit} className="add-ingredient-form">
+            <div>
+                {
+                    <h1>{determineText()}</h1>
+                }
+            </div>
+            <form action={AddIngredeient} className="add-ingredient-form">
                 <input
                     className="text"
                     type="text"
@@ -35,12 +63,78 @@ export default function Body() {
                     value="+ Add Ingredient"
                 />
             </form>
-            <div className="add-ingredient">
-                <h3>Ingredient on Hand</h3>
-                {ListIngrediwnt}
-            </div>
+            <section>
+                <h2>Ingre dient on Hand</h2>
+                <ul className="ingredients-list">{ListIngrediwnt}</ul>
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a Recipe</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                </div>
+            </section>
+            <section className="form">
+                <h1 className="signup">SignUp Form</h1>
+                <form action={signup}>
+                    <label htmlFor="email">Enter Email
+                    </label>
+                    <input id="email" defaultValue="joe@moe.com" name="email" type="email" />
+
+                    <label htmlFor="password">Enter password
+                    </label>
+                    <input defaultValue="password123" id="password" name="password" type="password" />
+                    <label htmlFor="description">Description</label>
+                    <textarea name="description" id="description" defaultValue="This is the description"></textarea>
+                    <fieldset>
+                        <legend>Employment Status:</legend>
+                        <label>
+                            <input type="radio" name="empstatus"
+                                value="unemployed" /> Unemployed
+                        </label>
 
 
+                        <label>
+                            <input type="radio" name="empstatus"
+                                value="employed" /> Employed
+                        </label>
+
+
+                        <label>
+                            <input defaultChecked={true} type="radio" name="empstatus"
+                                value="part-time" /> Part-Time
+                        </label>
+                    </fieldset>
+
+                    <fieldset>
+                        <legend>Employment Status:</legend>
+                        <label>
+                            <input type="checkbox" name="dietry"
+                                value="Gluten Free" /> Gluten Free
+                        </label>
+
+
+                        <label>
+                            <input type="checkbox" name="dietry"
+                                value="Vegan" /> Vegan
+                        </label>
+
+
+                        <label>
+                            <input defaultChecked={true} type="checkbox" name="dietry"
+                                value="Bakery" /> Bakery
+                        </label>
+                    </fieldset>
+
+                    <label htmlFor="favColor">What is your favorite color?</label>
+                    <select id="favColor" name="favColor" defaultValue="" required>
+                        <option value="">----Choose Color----</option>
+                        <option value="red">Red</option>
+                        <option value="orange">Orange</option>
+                        <option value="Green">Green</option>
+                    </select>
+                    <button>Submit</button>
+                </form>
+            </section>
         </>
     );
 }
